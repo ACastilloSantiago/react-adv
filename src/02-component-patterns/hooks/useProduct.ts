f7695ff4b7e2ada031b29d489e8cb43281ff9fcd
ACastilloSantiago <santiagorodrigec@gmail.com>
2024-08-22 09:41:10 -0300

    feat: :sparkles: update hook

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,34 +1,43 @@
-import { useEffect, useState } from 'react'
-import { onChangeArgs, Product } from '../interfaces/interfaces';
-
+import { useEffect, useRef, useState } from 'react';
+import { InitialValues, onChangeArgs, Product } from '../interfaces/interfaces';
 
 interface useProductArgs {
-    product: Product;
-    onChange?: ( args: onChangeArgs ) => void;
-    value?: number;
+  product: Product;
+  onChange?: (args: onChangeArgs) => void;
+  value?: number;
+  initialValues?: InitialValues;
 }
 
+export const useProduct = ({ onChange, product, value = 0, initialValues }: useProductArgs) => {
+  const [counter, setCounter] = useState<number>(initialValues?.count || value);
 
-export const useProduct = ({ onChange, product, value = 0 }: useProductArgs) => {
+  console.log(initialValues?.count);
 
-    const [ counter, setCounter ] = useState( value );
+  const isMounted = useRef(false);
 
+  const increaseBy = (value: number) => {
+    let newValue = Math.max(counter + value, 0);
 
-    const increaseBy = ( value: number ) => {
-      
-        const newValue = Math.max( counter + value, 0 )
-        setCounter( newValue );
-
-        onChange && onChange({ count: newValue, product });
+    if (initialValues?.maxCount) {
+      newValue = Math.min(newValue, initialValues.maxCount);
     }
 
-    useEffect(() => {
-        setCounter( value );
-    }, [ value ])
+    setCounter(newValue);
 
-    return {
-        counter,
-        increaseBy
-    }
+    onChange && onChange({ count: newValue, product });
+  };
 
-}
+  useEffect(() => {
+    if (!isMounted.current) return;
+    setCounter(value);
+  }, [value]);
+
+  useEffect(() => {
+    isMounted.current = true;
+  }, []);
+
+  return {
+    counter,
+    increaseBy,
+  };
+};
