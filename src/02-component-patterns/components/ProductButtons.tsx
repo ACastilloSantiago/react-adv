caf5503eaed86d4c040c56f5558a4e9d050bbf86
ACastilloSantiago <santiagorodrigec@gmail.com>
2024-08-22 10:58:58 -0300

    feat: :sparkles: update Buttons

--- src/02-component-patterns/components/ProductButtons.tsx
+++ src/02-component-patterns/components/ProductButtons.tsx
@@ -1,31 +1,48 @@
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
 import { ProductContext } from "./ProductCard";
 
 import styles from '../styles/styles.module.css'
 
 export interface Props {
     className?: string;
-    style?: React.CSSProperties 
+    style?: React.CSSProperties
 }
 
 export const ProductButtons = ({ className, style }: Props) => {
 
-    const { increaseBy, counter } = useContext( ProductContext );
+
+
+    // TODO: maxCount,
+
+    const { increaseBy, counter, maxCount } = useContext(ProductContext);
+
+    // TODO: isMaxReached = useCallback, dependencies [counter, maxCount]
+
+    const isMaxReached = useCallback(
+        () => !!maxCount && counter === maxCount ,
+        [counter, maxCount],
+    )
+
+
+    // TRUE si el count === maxCount
+    // False 
 
     return (
-        <div 
-            className={ `${ styles.buttonsContainer} ${ className }` }
-            style={ style }
+        <div
+            className={`${styles.buttonsContainer} ${className}`}
+            style={style}
         >
             <button
-                className={ styles.buttonMinus }
-                onClick={ () => increaseBy( -1 ) }> - </button>
+                className={styles.buttonMinus}
+                onClick={() => increaseBy(-1)}> - </button>
 
-            <div className={ styles.countLabel }> { counter } </div>
+            <div className={styles.countLabel}> {counter} </div>
 
             <button
-                className={ styles.buttonAdd }
-                onClick={ () => increaseBy( +1 ) }> + </button>
+                className={`${styles.buttonAdd} ${isMaxReached() && styles.disabled} `}
+                onClick={() => increaseBy(+1)}
+                disabled={isMaxReached()}
+            > + </button>
         </div>
     );
 }
